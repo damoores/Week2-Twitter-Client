b58632b57ad628284dd57193f11a0bc032f06b74
Jeremy Moore <damoores@gmail.com>
2016-06-13 16:09:28 -0700

    fix storyboard

--- TwitterClone/TwitterClone/ViewController.swift
+++ TwitterClone/TwitterClone/ViewController.swift
@@ -22,14 +22,6 @@ class ViewController: UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-//        JSONParser.tweetJSONFrom(JSONParser.JSONData()) { (success, tweets) in
-//            if success {
-//                for tweet in tweets! {
-//                    print(tweet.text)
-//                }
-//            }
-//        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -38,6 +30,7 @@ class ViewController: UIViewController
     }
     
     override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         JSONParser.tweetJSONFrom(JSONParser.JSONData()) { (success, tweets)  in
             if success {
                 if let tweets = tweets {
