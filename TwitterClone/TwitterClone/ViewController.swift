41fb0bdf549a0cc9df8ae68894f95161b758f087
Jeremy Moore <damoores@gmail.com>
2016-06-13 13:16:22 -0700

    initial commit

---
+++ TwitterClone/TwitterClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Jeremy Moore on 6/13/16.
+//  Copyright © 2016 Jeremy Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
